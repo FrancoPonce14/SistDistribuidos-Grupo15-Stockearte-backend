bcad92bac79265007a36f1a171aef486ac2682ef
Franco Ponce <francoponce20@hotmail.com>
2024-09-10 00:32:20 -0300

    carga server principal

---
+++ server/stockearte/src/main/java/com/stockearte/StockearteApplication.java
@@ -0,0 +1,13 @@
+package com.stockearte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockearteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockearteApplication.class, args);
+	}
+
+}
---
+++ server/stockearte/src/test/java/com/stockearte/StockearteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockearte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockearteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
