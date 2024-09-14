905d386862f7e75607ed991155747280795aac53
FrancoPonce14 <francoponce20@hotmail.com>
2024-09-14 18:26:05 -0300

    querys nativas

--- server/src/main/java/com/server/repositories/IProductoRepository.java
+++ server/src/main/java/com/server/repositories/IProductoRepository.java
@@ -1,9 +1,34 @@
 package com.server.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.server.entities.Producto;
 
 public interface IProductoRepository extends JpaRepository<Producto, Long> {
+
+        
+    @Query(value = "SELECT p.* FROM Producto p JOIN Stock s ON s.producto_id = p.id WHERE s.tienda_id = :tiendaId", nativeQuery = true)
+    public List<Producto> findByTienda(@Param("tiendaId") Long tiendaId);
+    
+
+    @Query(value = "SELECT p.* FROM producto p JOIN stock s ON p.id = s.producto_id WHERE s.tienda_id = :tiendaId AND "
+        + "(:nombre IS NULL OR LOWER(p.nombre) LIKE LOWER(CONCAT('%', :nombre, '%'))) AND "
+        + "(:codigo IS NULL OR LOWER(p.codigo) LIKE LOWER(CONCAT('%', :codigo, '%'))) AND "
+        + "(:talle IS NULL OR LOWER(p.talle) LIKE LOWER(CONCAT('%', :talle, '%'))) AND "
+        + "(:color IS NULL OR LOWER(p.color) LIKE LOWER(CONCAT('%', :color, '%')))", nativeQuery = true)
+    public Page<Producto> findByTienda(@Param("tiendaId") Long tiendaId, @Param("nombre") String nombre, @Param("codigo") String codigo, @Param("talle") String talle, @Param("color") String color, Pageable pageable);
+
+    @Query(value = "SELECT * FROM producto p WHERE "
+    + "(:nombre IS NULL OR LOWER(p.nombre) LIKE LOWER(CONCAT('%', :nombre, '%'))) AND "
+    + "(:codigo IS NULL OR LOWER(p.codigo) LIKE LOWER(CONCAT('%', :codigo, '%'))) AND "
+    + "(:talle IS NULL OR LOWER(p.talle) LIKE LOWER(CONCAT('%', :talle, '%'))) AND "
+    + "(:color IS NULL OR LOWER(p.color) LIKE LOWER(CONCAT('%', :color, '%')))", nativeQuery = true)
+    public Page<Producto> findAll(@Param("nombre") String nombre, @Param("codigo") String codigo, @Param("talle") String talle, @Param("color") String color, Pageable pageable);
     
 }
--- server/src/main/java/com/server/repositories/ITiendaRepository.java
+++ server/src/main/java/com/server/repositories/ITiendaRepository.java
@@ -1,9 +1,20 @@
 package com.server.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.server.entities.Tienda;
 
 public interface ITiendaRepository extends JpaRepository<Tienda, Long>{
     
+        public Tienda findByCodigo(String codigo);
+
+        @Query("SELECT t FROM Tienda t WHERE "
+        + "(:codigo IS NULL OR LOWER(t.codigo) LIKE LOWER(CONCAT('%', :codigo, '%'))) AND "
+        + "(:habilitado IS NULL OR t.habilitado = :habilitado)")
+        public Page<Tienda> findAll(@Param("codigo") String codigo, @Param("habilitado") Boolean habilitado, Pageable pageable);
+ 
 }
