ff5d4908b9b16cf6e29e3f115060a47b9363d847
FrancoPonce14 <francoponce20@hotmail.com>
2024-09-14 18:26:49 -0300

    query jpa para stock

--- server/src/main/java/com/server/repositories/IStockRepository.java
+++ server/src/main/java/com/server/repositories/IStockRepository.java
@@ -3,7 +3,11 @@ package com.server.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.server.entities.Stock;
+import com.server.entities.Tienda;
+import com.server.entities.Producto;
 
 public interface IStockRepository extends JpaRepository<Stock, Long> {
+
+    public Stock findByTiendaAndProducto(Tienda tienda, Producto producto);
     
 }
