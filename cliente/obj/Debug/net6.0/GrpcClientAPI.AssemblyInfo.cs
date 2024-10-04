7f280e06e863a5240c38538dfdaf5a10c01cd99d
FrancoPonce14 <francoponce20@hotmail.com>
2024-10-04 19:17:11 -0300

    ajuste

--- cliente/obj/Debug/net6.0/GrpcClientAPI.AssemblyInfo.cs
+++ cliente/obj/Debug/net6.0/GrpcClientAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrpcClientAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5545c34185597f166a921ebd2bc6788d4a5f5c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1f76b8678511e239a17791f4ddc1fd11942a72f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrpcClientAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrpcClientAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
